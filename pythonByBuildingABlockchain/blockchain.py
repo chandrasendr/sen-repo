3dff148659d15ece3821423498d5a47d3229ac8f
chandrasen <chandrasendr1992@gmail.com>
2019-10-20 19:11:09 +0200

    Comitted after finishing 64.Transactions with Dictionaries & Tuples

--- pythonByBuildingABlockchain/blockchain.py
+++ pythonByBuildingABlockchain/blockchain.py
@@ -1,5 +1,7 @@
 # Initializing our (empty) blockchain list
 blockchain = []
+open_transactions = []
+owner = 'Chandra'
 
 
 def get_last_blockchain_value():
@@ -12,23 +14,34 @@ def get_last_blockchain_value():
 # This function accepts two arguments.
 # First one required one (transaction _amount) and one optional one (last_transaction)
 # The optional one is optional because it has a default value => [1]
-def add_transaction(transaction_amount, last_transaction=[1]):
+def add_transaction(sender, recipient, amount=1.0):
     """ Append a new value as well as the last blockchain value to the blockchain
 
     Arguments:
-        :transaction_amount: The amount that should be added
-        :last_transaction: The last blockchain transaction (default[1])
+        :sender: The sender of the coins
+        :reciepient: The recipient of the coins
+        :amount: The amount of coins sent with the transaction (default = 1.0)
     """
-    if last_transaction == None:
-        last_transaction = [1]
-    blockchain.append([last_transaction, transaction_amount])
+    transaction = {
+        'sender': sender, 
+        'recipient': recipient, 
+        'amount': amount
+    }
+    open_transactions.append(transaction)
+    
+
+
+def mine_block():
+    pass
 
 
 def get_trasaction_value():
     """ Returns the input of the user (a new transaction amount) as a float. """
     # Get the user input, transform it from a string to a float and store it
-    user_input = float(input('Your transaction amount please: '))
-    return user_input
+    tx_recipient = input('Enter the recipient of the transaction: ')
+    tx_amount = float(input('Your transaction amount please: '))
+    # This creates a Tuple. If you want to create a Tuple with only one value then you write (tx_recipient, )
+    return tx_recipient, tx_amount
 
 
 def get_user_choice():
@@ -83,7 +96,7 @@ while waiting_for_input:
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
-        tx_amount = get_trasaction_value()
+        tx_data = get_trasaction_value()
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
@@ -103,4 +116,3 @@ else:
 
 
 print('Done!')
-  
