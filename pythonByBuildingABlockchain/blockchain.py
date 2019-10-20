95f705c4ff77660489ca5818d3d9f252e5b5bc91
chandrasen <chandrasendr1992@gmail.com>
2019-10-20 19:57:38 +0200

    Comitted after finishing 65.Unpacking the tuples

--- pythonByBuildingABlockchain/blockchain.py
+++ pythonByBuildingABlockchain/blockchain.py
@@ -14,7 +14,7 @@ def get_last_blockchain_value():
 # This function accepts two arguments.
 # First one required one (transaction _amount) and one optional one (last_transaction)
 # The optional one is optional because it has a default value => [1]
-def add_transaction(sender, recipient, amount=1.0):
+def add_transaction(srecipient, sender=owner, amount=1.0):
     """ Append a new value as well as the last blockchain value to the blockchain
 
     Arguments:
@@ -97,7 +97,9 @@ while waiting_for_input:
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_data = get_trasaction_value()
-        add_transaction(tx_amount, get_last_blockchain_value())
+        recipient, amount = tx_data
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice == 'h':
