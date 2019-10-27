4ccaa5de4bcc0d9d2e74913d0f2105f44f3e3442
chandrasen <chandrasendr1992@gmail.com>
2019-10-27 22:52:26 +0100

    Committing after finising 75. Making a list of participants in the Blockchain

--- pythonByBuildingABlockchain/blockchain.py
+++ pythonByBuildingABlockchain/blockchain.py
@@ -7,6 +7,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Chandra'
+participants = {'Chandra'}
 
 
 def hash_block(block):
@@ -37,6 +38,8 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         'amount': amount
     }
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
     
 
 def mine_block():
@@ -95,6 +98,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output participants')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -108,7 +112,10 @@ while waiting_for_input:
         mine_block()
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
+        # Make sure that you don't try to "hack" the bockchain if it's empty
         if len(blockchain) >= 1:
             blockchain[0] = {
             'previous_hash': '',
