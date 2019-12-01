cb804b9a5fb124e74d920e06bb7a7ed38c2af22e
chandrasen <chandrasendr1992@gmail.com>
2019-12-01 21:40:32 +0100

    committing after finishing 97. fixing a bug and also have commented some code

--- pythonByBuildingABlockchain/blockchain.py
+++ pythonByBuildingABlockchain/blockchain.py
@@ -1,33 +1,48 @@
 import functools
-# Initializing our (empty) blockchain list
+# The reward we give to miners (for creating a new block)
 MINING_REWARD = 10
 
+# Our starting block for the blockchain
 genesis_block = {
     'previous_hash': '',
     'index': 0,
     'transactions': []
 }
+# Initializing our (empty) blockchain list
 blockchain = [genesis_block]
+# Unhandeled transactions
 open_transactions = []
+# We are the owner of this blockchain node, hence this is our identifier (eg. Chandra)
 owner = 'Chandra'
+# registered participants: Ourself + other people sendind/ receiving coins
 participants = {'Chandra'}
 
 
 def hash_block(block):
+    """Hashes a block and returns a string reepresentation of it.
+
+    Arguments:
+        :block: The block that should be hashed.
+    """
     return '-'.join([str(block[key]) for key in block])
 
 
 def get_balance(participant):
+    """Calculate and return the balance for a participation
+
+    Arguments:
+        :participent: The person for whom to calculate the balance
+    """
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
     # This fetches sent amounts of open transactions (to avoid double spending )
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
     # This fetches received coin amounts of transactions that were already included in the blocks of the blockchain
     # We ignore open transactions here because you shouldn't be able to spend coins before the transaction was confirmed + included 
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_recieved = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)   
+    amount_recieved = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)   
     # Return the total balance
     return amount_recieved - amount_sent
 
